f3ca263abe83d2fd344c39c9173de2cb7d0ef000
Mhando-Z <mhandosz17@gmail.com>
2026-06-29 18:12:24 +0300

    Phase One

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, createContext } from "react";
 const DataContext = createContext();
 
 export function DataProvider({ children }) {
-  const [data, setData] = usestate();
+  const [data, setData] = useState();
 
   return <DataContext.Provider>{children}</DataContext.Provider>;
 }
