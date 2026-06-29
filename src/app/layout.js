987d84685c9dc407cbf727ea8da8c0f9ca5518bd
Mhando-Z <mhandosz17@gmail.com>
2026-06-29 18:39:27 +0300

    Phase One

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { DataProvider } from "@/context/DataContext";
 
 export const metadata = {
   title: "MhdTv",
@@ -20,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`min-h-full antialiased overflow-x-hidden`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <DataProvider>{children}</DataProvider>
+      </body>
     </html>
   );
 }
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,21 +1,58 @@
+"use client";
+
 import { useState, useEffect, createContext } from "react";
 
 const DataContext = createContext();
 
 export function DataProvider({ children }) {
   const [channels, setChannels] = useState();
+  const [feeds, setFeeds] = useState();
+  const [logos, setLogos] = useState();
+  const [stream, setStream] = useState();
+
+  const fetchStreamsds = async () => {
+    try {
+      fetch("https://iptv-org.github.io/api/streams.json")
+        .then((res) => res.json())
+        .then((data) => {
+          setStream(data);
+        });
+    } catch {}
+  };
+  const fetchFeeds = async () => {
+    try {
+      fetch("https://iptv-org.github.io/api/feeds.json")
+        .then((res) => res.json())
+        .then((data) => {
+          setFeeds(data);
+        });
+    } catch {}
+  };
+  const fetchLogos = async () => {
+    try {
+      fetch("https://iptv-org.github.io/api/logos.json")
+        .then((res) => res.json())
+        .then((data) => {
+          setLogos(data);
+        });
+    } catch {}
+  };
 
   const fetchChannels = async () => {
-    fetch("https://iptv-org.github.io/api/channels.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setChannels(data);
-      });
-    return null;
+    try {
+      fetch("https://iptv-org.github.io/api/channels.json")
+        .then((res) => res.json())
+        .then((data) => {
+          setChannels(data);
+        });
+    } catch {}
   };
 
   useEffect(() => {
     fetchChannels();
+    fetchStreamsds();
+    fetchFeeds();
+    fetchLogos();
   }, []);
 
   return (
