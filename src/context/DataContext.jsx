6076f66ed24bf308dcc6972ce8cae0bfe574c4c2
Mhando-Z <mhandosz17@gmail.com>
2026-06-29 16:36:04 +0300

    Phase One

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,11 @@
+// const DataContext = useContext()
+
+// export function DataProvider({children}){
+//     const [data,setData]=usestate()
+
+//     return(
+
+//     )
+// }
+
+// export default DataContext
